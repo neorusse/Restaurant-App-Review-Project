acbc4f61fd50d79c39db56ba6322b5665a9715af
Russell Nyorere <ecodenhub@gmail.com>
2018-10-10 05:40:56 +0100

    bug fix in file

--- sevw.js
+++ sevw.js
@@ -1,46 +1,63 @@
-self.addEventListener('install', function (evt) {
-    evt.waitUntil(
-        caches.open('staticCacheName').then(function (cache) {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'restaurant.html',
-                '/css/styles.css',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/data/restaurants.json',
-                '/img/1.jpg',
-                '/img/2.jpg',
-                '/img/3.jpg',
-                '/img/4.jpg',
-                '/img/5.jpg',
-                '/img/6.jpg',
-                '/img/7.jpg',
-                '/img/8.jpg',
-                '/img/9.jpg',
-                '/img/10.jpg'
-            ]);
-        })
-    );
+/**
+ * Varriable created for cache file names 
+ */
+
+const urlsToCache = [
+        '/',
+        'index.html',
+        'restaurant.html',
+        '/css/styles.css',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+    ];
+
+/**
+ * Service Worker Installation
+*/
+
+
+self.addEventListener('install', function(evt) {
+// Perform install steps
+evt.waitUntil(
+    caches.open('cacheName')
+    .then(function(cache) {
+        return cache.addAll(urlsToCache);
+    })
+);
 });
 
+/**
+ *  Add fetch events to serviceWorker 
+ */
+
 self.addEventListener('fetch', function(evt) {
-    evt.respondWith(
-        caches.match(evt.req).then(function(res) {
-            if (res) {
+    e.respondWith(
+        caches.match(evt.request).then(function(response) {
+            if (response) {
                 console.log(evt.req, 'is in cache');
-                return res;
+                return response;
             }
             else {
                 console.log(evt.req, 'is not fouund in cache');
-                return fetch(evt.req)
-                .then(function(res) {
-                    const responseClone = res.clone();
-                    caches.open('staticCacheName').then(function(cache) {
-                        cache.put(evt.req, responseClone);
+                return fetch(evt.request)
+                .then(function(response) {
+                    const clonedResponse = response.clone(); 
+                    caches.open('cacheName').then(function(cache) {
+                        cache.put(evt.request, clonedResponse);
                     })
-                    return res;
+                    return response;
                 })
                 .catch(function(err) {
                     console.error(err);
